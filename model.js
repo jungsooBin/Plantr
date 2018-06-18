3de1e4a121eb20e60495da3df75e29293c87d0e3
Jungsoo Bin <witberrybean@gmail.com>
2018-06-18 17:26:28 -0400

    jungsoo

--- model.js
+++ model.js
@@ -2,6 +2,45 @@ const pg = require('pg')
 const Sequelize = require('sequelize')
 const db = new Sequelize('postgres://localhost:5432/plantr')
 
+const Vegetable = db.define("vegetable", {
+  name: {
+    type: Sequelize.STRING
+  },
+  color: {
+    type: Sequelize.STRING
+  },
+  planted_on: {
+    type: Sequelize.DATE
+  }
+})
+
+const Gardener = db.define("gardener", {
+  name : {
+    type: Sequelize.STRING
+  },
+  age : {
+    type: Sequelize.INTEGER
+  }
+})
+
+const Plot = db.define("plot", {
+  size: {
+    type: Sequelize.INTEGER
+  },
+  shaded: {
+    type: Sequelize.BOOLEAN
+  }
+})
+
+
+
+Plot.belongsTo(Gardener)
+Gardener.hasMany(Plot)
+Vegetable.belongsToMany(Plot, {through: 'vegePlot'})
+Plot.belongsToMany(Vegetable, {through: 'vegePlot'})
+Gardener.belongsTo(Vegetable, {as: 'favorite_vegetable'})
+
+
 
 db.sync({force: true})
   .then(() => {
@@ -24,4 +63,4 @@ db.sync({force: true})
 
 
 
-module.exports = db
+module.exports = {db, Vegetable,Gardener, Plot }
---
+++ seed.js
@@ -0,0 +1,3 @@
+const {model} = require('./model')
+
+// const zuc = Vegetable.create({name: 'zucchini', color: 'green', planted_on: date()})
